6001ffff8c4a8b47317fde79a43e4c0bc2e3e56f
s-khechnev <hechnev@gmail.com>
2023-03-20 20:28:58 +0300

    add TowerAttributeBars - bars for visualize level attribute

---
+++ Assets/Scripts/Defender/HUD/Bar/TowerAttributeBar.cs
@@ -0,0 +1,21 @@
+﻿using Data.Towers;
+
+namespace Defender.HUD.Bar
+{
+    public class RangeBar<T> : Bar
+    {
+        private TowerData _towerData;
+        private TowerAttribute<float> _range;
+        
+        public void Init(TowerData towerData)
+        {
+            _towerData = towerData;
+            _range.LevelChanged += OnRangeLevelChanged;
+        }
+
+        private void OnRangeLevelChanged(int level, int maxLevel)
+        {
+            ChangeValue(level, maxLevel);
+        }
+    }
+}
